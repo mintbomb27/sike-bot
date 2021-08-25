bfdb36708fb86ccd6066a9b4358974f9a07968df
Alok <alokme123@gmail.com>
2021-08-26 00:37:39 +0530

    rm: commands dir

--- commands/youtube.js
+++
@@ -1,7 +0,0 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-
-module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('youtube')
-		.setDescription('Creates Youtube Together Link!'),
-};
